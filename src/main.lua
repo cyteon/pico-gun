5e13c1edee086aa9e56e3fb413e815b0ac3f170d
cyteon <cyteon@hackclub.app>
2025-07-15 18:12:45 +0200

    init, drawing sprite works

---
+++ src/main.lua
@@ -0,0 +1,11 @@
+--[[pod_format="raw",created="2025-07-15 13:06:56",modified="2025-07-15 16:05:58",revision=80]]
+function _init()
+	sheet = fetch("lib/0.gfx")
+	layers = fetch("lib/0.map")
+end
+
+function _draw()
+  	cls()
+  	map(layers[1].bmp)
+   spr(sheet[1].bmp, 0, 0, false)
+end
