944d00a5f2212fe9f7ba50e108c62fd5b5bcf8a4
cyteon <cyteon@hackclub.app>
2025-07-20 18:52:47 +0200

    collision fixes

--- src/bullets.lua
+++ src/bullets.lua
@@ -1,4 +1,4 @@
---[[pod_format="raw",created="2025-07-17 06:51:51",modified="2025-07-17 07:50:27",revision=38]]
+--[[pod_format="raw",created="2025-07-17 06:51:51",modified="2025-07-20 16:49:41",revision=43]]
 function spawn_bullet(x, y, dir)
 	if (dir == 0) s = 7; h_flip = true; v_flip = false;
 	if (dir == 1) s = 7; h_flip = false; v_flip = false;
@@ -11,10 +11,10 @@ end
 function _bullets_update()
 	for bullet in all(bullets) do
 		-- despawn bullet on collision
-		if (bullet.dir == 0 and fget(mget(bullet.x \ 16, (bullet.y + 8) \ 16), 1)) del(bullets, bullet)
-		if (bullet.dir == 1 and fget(mget((bullet.x + 15) \ 16, (bullet.y + 8) \ 16), 1)) del(bullets, bullet)
-		if (bullet.dir == 2 and fget(mget((bullet.x + 8)  \ 16, bullet.y \ 16), 1)) del(bullets, bullet)
-		if (bullet.dir == 3 and fget(mget((bullet.x + 8)  \ 16, (bullet.y + 15) \ 16), 1)) del(bullets, bullet)
+		if (bullet.dir == 0 and collides_left(bullet.x, bullet.y)) del(bullets, bullet)
+		if (bullet.dir == 1 and collides_right(bullet.x, bullet.y)) del(bullets, bullet)
+		if (bullet.dir == 2 and collides_up(bullet.x, bullet.y)) del(bullets, bullet)
+		if (bullet.dir == 3 and collides_down(bullet.x, bullet.y)) del(bullets, bullet)
 
 		if (bullet.dir == 0) bullet.x -= 2
 		if (bullet.dir == 1) bullet.x += 2
--- src/utils.lua
+++ src/utils.lua
@@ -1,16 +1,16 @@
---[[pod_format="raw",created="2025-07-20 11:02:19",modified="2025-07-20 11:03:32",revision=6]]
+--[[pod_format="raw",created="2025-07-20 11:02:19",modified="2025-07-20 16:48:00",revision=17]]
 function collides_left(x, y)
-	return fget(mget((p.x - 1) \ 16, p.y \ 16), 1)
+	return fget(mget((x - 1) \ 16, y \ 16), 1)
 end
 
 function collides_right(x, y)
-	return fget(mget((p.x + 16) \ 16, p.y \ 16), 1)
+	return fget(mget((x + 16) \ 16, y \ 16), 1)
 end
 
 function collides_up(x, y)
-	return fget(mget(p.x \ 16, (p.y - 1) \ 16), 1)
+	return fget(mget(x \ 16, (y - 1) \ 16), 1)
 end
 
 function collides_down(x, y)
-	return fget(mget(p.x \ 16, (p.y + 16) \ 16), 1)
+	return fget(mget(x \ 16, (y + 16) \ 16), 1)
 end
