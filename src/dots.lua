e13a1e073e91d4e1224eb885322a19232a38fe33
cyteon <cyteon@hackclub.app>
2025-07-22 18:14:14 +0200

    randomly spawn hearts

--- src/dots.lua
+++ src/dots.lua
@@ -1,4 +1,4 @@
---[[pod_format="raw",created="2025-07-21 18:41:11",modified="2025-07-22 08:16:41",revision=40]]
+--[[pod_format="raw",created="2025-07-21 18:41:11",modified="2025-07-22 16:12:31",revision=53]]
 function _dots_init()
 	dots = {}
 	
@@ -6,8 +6,10 @@ function _dots_init()
 		for tx=28,1,-1 do
 		
 			if not fget(mget(tx, ty), flags.no_dots) then
-				local is_bullet = math.random(1,20) == 1
-				add(dots, { x = tx * 16, y = ty * 16, is_bullet = is_bullet })
+				local is_bullet = math.random(1,20) == 1 -- 5% chance
+				local is_heart = math.random(1,200) == 1 and not is_bullet -- <0.5% chance	
+			
+				add(dots, { x = tx * 16, y = ty * 16, is_bullet = is_bullet, is_heart = is_heart })
 			end
 		end
 	end
@@ -17,6 +19,7 @@ function _dots_update()
 	for dot in all(dots) do
 		if spr_collides(dot.x + 6, dot.y + 6, 2, 2, p.x + 1, p.y + 1, 15, 15) then
 			if dot.is_bullet then p.ammo += 1
+			elseif dot.is_heart then p.hp += 1
 			else p.score += 10
 			end
 		
@@ -29,8 +32,9 @@ function _dots_update()
 		local tx = math.random(1,28)	
 
 		if not fget(mget(tx, ty), flags.no_dots) then
-			local is_bullet = math.random(1,20) == 1
-			add(dots, { x = tx * 16, y = ty * 16, is_bullet = is_bullet })
+			local is_bullet = math.random(1,20) == 1 -- 5% chance
+			local is_heart = math.random(1,200) == 1 and not is_bullet -- <0.5% chance	
+			add(dots, { x = tx * 16, y = ty * 16, is_bullet = is_bullet, is_heart = is_heart})
 		end
 	end
 end
@@ -38,6 +42,7 @@ end
 function _dots_draw()
 	for dot in all(dots) do
 		if dot.is_bullet then spr(6, dot.x, dot.y)
+		elseif dot.is_heart then spr(15, dot.x, dot.y)
 		else spr(14, dot.x, dot.y)
 		end
 	end
